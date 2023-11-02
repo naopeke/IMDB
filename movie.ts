9733869aac1572c2bfcdac15f71db1d3f61428b1
Belen Castelli <mariabelencastelli29@gmail.com>
2023-11-02 11:15:22 +0100

    paso 3 y 4

---
+++ movie.ts
@@ -0,0 +1,33 @@
+import { Professional } from "./professional"
+
+class Movie {
+    public tittle:string
+    public releaseYear: number
+    public actors: Professional[]
+    public nacionality: string
+    public director: Professional
+    public writer: Professional
+    public language: string
+    public plataforma: string
+    public isMCU: boolean
+    public mainCharacterName: string
+    public producer: string
+    public distributor: string
+    public genre: string
+
+    constructor (title:string, releaseYear:number, nationality:string, genre:string) {
+        this.tittle = title
+        this.releaseYear = releaseYear
+        this.nacionality = nationality
+        this.genre = genre
+    }
+
+    public printAll ():void {
+        for(let propiedad in this){
+            if(typeof this[propiedad] != "function")
+            console.log(`${propiedad}: ${this[propiedad]}`);
+    }
+}
+}
+
+export {Movie}
---
+++ testMovie.ts
@@ -0,0 +1,19 @@
+import { Professional } from "./professional";
+import { Movie } from "./movie";
+
+let professional1 = new Professional ("Ewan Mcgregor",52,80,177,false,"Británica",0,"actor")
+let professional2 = new Professional ("Danny Boyle", 67, 80, 184, false, "británico", 1, "Director")
+let professional3 = new Professional ("Irvin Welsh", 65, 80, 182, false, "britanica", 0, "escritor")
+let movie1 = new Movie("Trainspotting",1996,"británica","drama/crimen")
+
+movie1.actors = [professional1]
+movie1.director = professional2
+movie1.writer = professional3
+movie1.language = "English"
+movie1.language = "Prime Video"
+movie1.isMCU = false
+movie1.mainCharacterName = "Mark Renton"
+movie1.producer = "Andrew Macdonald"
+movie1.distributor = "PolyGram Filmed Entertainment"
+
+movie1.printAll()
