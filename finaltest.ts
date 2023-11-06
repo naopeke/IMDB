ffa7dfbf63cda13386063b8839e48439b071728f
Belen Castelli <mariabelencastelli29@gmail.com>
2023-11-06 09:36:55 +0100

    integration

--- finaltest.ts
+++ finaltest.ts
@@ -1,34 +1,43 @@
-import { Professional } from './professional'
-import { Movie } from './movie'
-import { Imdb } from './Imdb'
+import { Professional } from "./professional";
+import { Movie } from "./movie";
+import { Imdb } from "./imdb";
+import { log } from "console";
 
-//readLineSync
-const readlineSync = require('readline-sync');
 
-let inputName = readlineSync.question('Enter name: ');
-let inputAge =  readlineSync.question('Enter age: ');
-let inputWeight =  readlineSync.question('Enter weight: ');
-let inputHeight = readlineSync.question('Enter height: ');
-let inputRetired =  readlineSync.question('Enter if he/she is retired or not: ');
-let inputNationality =  readlineSync.question('Enter nationality: ');
-let inputOscarNumber =  readlineSync.question('Enter numbers of Oscar: ');
-let inputProfession =  readlineSync.question('Enter profession: ');
 
-inputAge = parseFloat(inputAge);
-inputWeight = parseFloat(inputWeight);
-inputHeight = parseFloat(inputHeight);
-inputOscarNumber = parseFloat(inputOscarNumber);
 
-const fs = require('fs');
+// readLineSync
 
-//exiting data
-let data = JSON.parse(fs.readFileSync('imdbBBDD.json', 'utf-8'));
+let readLineSync = require("readline-sync")
 
-//new data
-let newProfessional = new Professional (inputName, inputAge, inputWeight, inputHeight, inputRetired, inputNationality, inputOscarNumber, inputProfession)
+// solicite por pantalla cada uno de las propiedades 
+// de la clase Profesional
 
-data.push(newProfessional);
-fs.writeFileSync('imdbBBDD.json', JSON.stringify(data, null, 5));
-//JSON.stringify(value, replacer, space);
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+let name:string = readLineSync.question("What is your name?")
+let age:number = readLineSync.question("How old are you?")
+let weight: number = readLineSync.question("How much do you weigh?")
+let height: number = readLineSync.question("How tall are you?")
+let isRetired:boolean = readLineSync.question("Are you retired?")
+let nationality:string = readLineSync.question("What is your nationality?")
+let oscarNumber:number = readLineSync.question("How many Oscars do you have?")
+let profession:string = readLineSync.question("What is your profession?")
+
+// inserte un nuevo profesional a una película del fichero 
+// “imdbBBDD.json
+
+let professional1 = new Professional ("Ewan Mcgregor",52,80,177,false,"Británica",0,"actor")
+let profesional4 = new Professional (name, age, weight, height,isRetired,nationality,oscarNumber,profession)
+let movie1 = new Movie("Trainspotting",1996,"británica","drama/crimen")
+movie1.actors = [professional1]
+let imdb1 = new Imdb ([movie1])
+
+
+let json = imdb1.escribirEnFicheroJSON("imdbBBDD2")
+imdb1.obtenerInstanciaIMDB("imdbBBDD2.json");
+imdb1.movies[0].actors.push(profesional4)
+imdb1.escribirEnFicheroJSON("imdbBBDD2")
+
+
+
+
 
